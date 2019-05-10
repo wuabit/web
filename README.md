685d1685e6b97d8156a7ea58f13dc4f6e3d7672a
mrmx <mistermx@gmail.com>
2019-05-10 21:02:47 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Wuabit.com
---
+++ js/msgs.js
@@ -0,0 +1,38 @@
+var messages = {
+	"en" : {
+		"menu-language": "Language",
+		"intro" : "Bitcoin and other cryptos within your favorite messaging app!",
+		"intro-typed" : "$1 within $2!",
+		"signup" : "Sign up!",
+		"signup-header" : "Subscribe to our mailing list!",
+		"signup-desc" : "Be the first to enter Wuabit's beta!",
+		"email-placeholder" : "Enter your email...",
+		"feature-blockchain-title" : "Blockchain meets IM",
+		"feature-blockchain-desc" : "Wuabit will allow to access current and future blockchain services from your chat app, from market prices to smart-contracts",
+		"feature-social-title" : "Chat and social interaction",
+		"feature-social-desc" : "As easy you chat with a friend you can chat with your Wuabit assistant. Ask him to pay your last dinner with friends or let your mom get her first Bitcoin wallet!",
+		"feature-multi-title" : "Use your favourite IM",
+		"feature-multi-desc" : "Beginning with the ubiquituous Whatsapp, more chat platforms will be added such as Telegram, FB Messenger, Viber",
+		"privacy-policy-title" : "Privacy policy",
+		"made-by" : "Made with ❤️ by",
+		"accept" : "Accept"		
+	},
+	"es" : {
+		"menu-language": "Idioma",
+		"intro" : "¡Bitcoin y otras criptomonedas en tu app de mensajería favorita!",
+		"intro-typed" : "¡$1 en $2!",
+		"signup" : "¡Apúntame!",
+		"signup-header" : "¡Subscríbete a nuestra lista de correo!",
+		"signup-desc" : "¡Sé el primero en entrar a la beta de Wuabit!",
+		"email-placeholder" : "Introduce tu email...",
+		"feature-blockchain-title" : "Blockchain en el chat",
+		"feature-blockchain-desc" : "Wuabit permitirá acceder a los servicios blockchain actuales y futuros desde tu aplicación de chat, desde precios de mercado hasta contratos inteligentes",
+		"feature-social-title" : "Chat e interacción social",
+		"feature-social-desc" : "Tan fácil como chateas con un amigo así puedes hacerlo con tu asistente Wuabit. ¡Pídele que pague la última cena con tus amigos o deja que tu mamá consiga su primera billetera de Bitcoin!",
+		"feature-multi-title" : "Usa tu chat favorito",
+		"feature-multi-desc" : "Comenzando con la plataforma más grande, Whatsapp, se agregarán más plataformas de chat como Telegram, FB Messenger y Viber",
+		"privacy-policy-title" : "Política de privacidad"	,
+		"made-by" : "Hecho con ❤️ por",
+		"accept" : "Acepto"			
+	}
+};
